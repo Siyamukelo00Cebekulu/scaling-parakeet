1436d25b742b258a4ffd47c8db2dd2269426b96f
Siyamukelo Cebekulu <siyamukelocebekulu@gmail.com>
2026-05-25 17:06:40 +0200

    chore:  registered refresh tokens and model configuration in the DbContext

--- backend/services/UserService/src/UserService.Api/Data/UserServiceDbContext.cs
+++ backend/services/UserService/src/UserService.Api/Data/UserServiceDbContext.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-
+using UserService.Api.Models;
 
 namespace UserService.Api.Data;
 
@@ -11,4 +11,17 @@ public class UserServiceDbContext : DbContext
     }
 
     public DbSet<User> Users { get; set; }
+
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.RefreshTokens)
+            .WithOne(rt => rt.User)
+            .HasForeignKey(rt => rt.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
